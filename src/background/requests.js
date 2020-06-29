6ca0ae1fbdb93348cce44dc728ea2ef3cbcd50e2
Yunzhe <yunzhe@zju.edu.cn>
2020-06-30 00:50:14 +0800

    Refactor requests: Introducing async generators.

--- src/background/requests.js
+++ src/background/requests.js
@@ -1,70 +1,63 @@
 /* eslint-disable no-await-in-loop */
-const getCurrentLives = async () => {
-  const limit = 20
-
-  let page = 0
-  let currentLives = []
-  let shouldContinue = true
-
-  while (shouldContinue) {
-    const response = await fetch(`https://holo.dev/api/v1/lives/current?limit=${limit}&page=${page}`)
-    if (!response.ok) {
-      throw new Error(`Network error: ${response.status}`)
-    }
-    const { lives } = await response.json()
-    currentLives = currentLives.concat(lives)
-
-    if (lives.length < limit) {
-      shouldContinue = false
-    } else {
-      page += 1
-    }
+const gatherResponse = async response => {
+  if (response.headers.get('content-type').includes('application/json')) {
+    return response.json()
   }
-
-  return currentLives
+  return response.text()
 }
 
-const getScheduledLives = async () => {
-  const limit = 20
+const fetchData = async (...args) => {
+  const response = await fetch(...args)
 
-  let page = 1
-  let scheduledLives = []
-  let shouldContinue = true
-
-  while (shouldContinue) {
-    const response = await fetch(`https://holo.dev/api/v1/lives/scheduled?limit=${limit}&page=${page}`)
-    if (!response.ok) {
-      throw new Error(`Network error: ${response.status}`)
-    }
-    const { lives } = await response.json()
-    scheduledLives = scheduledLives.concat(...lives)
-
-    if (lives.length < limit) {
-      shouldContinue = false
-    } else {
-      page += 1
-    }
-  }
-
-  return scheduledLives
-}
-
-const getChannels = async () => {
-  const response = await fetch('https://holo.dev/api/v1/channels?limit=100')
   if (!response.ok) {
     throw new Error(`Network error: ${response.status}`)
   }
-  const { channels } = await response.json()
+
+  return gatherResponse(response)
+}
+
+async function* liveFetcher(endpoint, options = {
+  limit: 20,
+}) {
+  const { limit = 20 } = options
+
+  let page = 0
+  let shouldContinue = true
+
+  do {
+    page += 1
+
+    const { lives } = await fetchData(`https://holo.dev/api/v1/lives/${endpoint}?limit=${limit}&page=${page}`)
+
+    yield lives
+
+    if (lives.length < limit) {
+      shouldContinue = false
+    }
+  } while (shouldContinue)
+}
+
+const fetchLives = async (...args) => {
+  const currentLives = []
+
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const lives of liveFetcher(...args)) {
+    currentLives.push(lives)
+  }
+
+  return currentLives.flat()
+}
+
+const getCurrentLives = () => fetchLives('current')
+
+const getScheduledLives = () => fetchLives('scheduled')
+
+const getChannels = async () => {
+  const { channels } = await fetchData('https://holo.dev/api/v1/channels?limit=100')
   return channels
 }
 
-const getMembers = async () => {
-  const response = await fetch('https://holo.dev/api/v1/members')
-  if (!response.ok) {
-    throw new Error(`Network error: ${response.status}`)
-  }
-  return response.json()
-}
+const getMembers = () => fetchData('https://holo.dev/api/v1/members')
 
 export {
   getCurrentLives,

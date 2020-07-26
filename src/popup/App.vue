ae132031e99a1c71eabbb4833de554950effabc7
Yunzhe <yunzhe@zju.edu.cn>
2020-07-26 13:04:05 +0800

    Update Settings page.

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="app">
     <div class="head">
-      <img class="logo" :src="popupLogo" alt="logo">
-      <HIcon class="icon" name="settings" @click="onClickSettings" />
+      <template v-if="route === 'main'">
+        <div class="main">
+          <img class="logo" :src="popupLogo" alt="logo">
+          <button type="button">
+            <HIcon class="icon" name="settings" @click="onClickSettings" />
+          </button>
+        </div>
+      </template>
+      <template v-if="route === 'settings'">
+        <div class="settings">
+          <button type="button" class="back" @click="onClickSettings">
+            <HIcon class="icon" name="back" />
+            <span class="text">Back</span>
+          </button>
+          <span class="title">Settings</span>
+        </div>
+      </template>
     </div>
     <div class="body">
       <div ref="main" class="main">
@@ -19,7 +34,26 @@
         <VHint class="hint" />
       </div>
       <div v-if="route === 'settings'" class="settings">
-        Settings
+        <div class="option">
+          <label class="label">
+            <span>Enable notifications</span>
+            <input type="checkbox" :checked="isNtfEnabled" @input="onChangeAlarmEnabled">
+          </label>
+          <div class="description">
+            Notify you by desktop notifications about the guerilla lives and reminders you’ve set.
+          </div>
+        </div>
+        <hr>
+        <div class="info">
+          <div class="engine">
+            Powered by
+            <a href="https://github.com/YunzheZJU/non-stop-story" target="_blank">Non-stop-story</a>
+          </div>
+          <div class="contact">
+            If you have any question, please contact
+            <a href="mailto:help@holo.dev" target="_blank">help@holo.dev</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -31,8 +65,12 @@
   import LiveListEnded from 'components/live-list-ended'
   import VHint from 'components/v-hint'
   import VToast from 'components/v-toast'
+  import { IS_NTF_ENABLED } from 'shared/store/keys'
   import { sleep } from 'utils'
+  import { mapState } from 'vuex'
   import browser from 'webextension-polyfill'
+
+  const { workflows: { toggleIsNtfEnabled } } = browser.extension.getBackgroundPage()
 
   const ratioThreshold = { high: 0.99, low: 0.01 }
 
@@ -61,6 +99,9 @@
       loadingConfig() {
         return this.LoadingStatuses.get(this.loadingStatus)
       },
+      ...mapState({
+        isNtfEnabled: IS_NTF_ENABLED,
+      }),
     },
     mounted() {
       const intersectionObserver = new IntersectionObserver(
@@ -122,6 +163,9 @@
       onClickSettings() {
         this.route = this.route === 'settings' ? 'main' : 'settings'
       },
+      onChangeAlarmEnabled() {
+        return toggleIsNtfEnabled()
+      },
     },
   }
 </script>
@@ -133,87 +177,163 @@
   }
 
   .head {
-    display: grid;
-    grid-auto-flow: column;
-    align-items: center;
-    justify-content: space-between;
     padding: 12px 16px;
     background: linear-gradient(45deg, #268C89, #65E5B4);
+    color: var(--color-text-white);
 
-    .logo {
-      height: 32px;
+    .main {
+      display: grid;
+      grid-auto-flow: column;
+      align-items: center;
+      justify-content: space-between;
+
+      .logo {
+        height: 32px;
+      }
+
+      .icon {
+        font-size: 24px;
+        cursor: pointer;
+
+        &:hover {
+          filter: brightness(0.9);
+        }
+      }
     }
 
-    .icon {
-      color: var(--color-text-white);
-      font-size: 24px;
-      cursor: pointer;
+    .settings {
+      display: grid;
+      grid-template-columns: 1fr auto 1fr;
+      align-items: center;
+      justify-content: space-between;
+      height: 32px;
 
-      &:hover {
-        filter: brightness(0.9);
+      .back {
+        display: grid;
+        grid-auto-flow: column;
+        align-items: center;
+        justify-content: start;
+        cursor: pointer;
+
+        &:hover {
+          filter: brightness(0.9);
+        }
+
+        .icon {
+          font-size: 24px;
+        }
+
+        .text {
+          font-size: 14px;
+        }
+      }
+
+      .title {
+        font-weight: 700;
+        font-size: 18px;
       }
     }
   }
 
   .body {
     position: relative;
-  }
 
-  .main {
-    overflow-y: auto;
-    max-height: 540px;
-    scrollbar-width: none;
+    .main {
+      overflow-y: auto;
+      max-height: 540px;
+      scrollbar-width: none;
 
-    &::-webkit-scrollbar {
-      display: none;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+
+    .loading {
+      display: grid;
+      grid-auto-flow: column;
+      gap: 4px;
+      align-items: center;
+      justify-content: center;
+      padding: 4px 0;
+      color: var(--color-text-light);
+      font-size: 14px;
+      cursor: pointer;
+
+      .icon {
+        font-size: 20px;
+      }
+    }
+
+    .scroll {
+      overflow-y: auto;
+      height: 540px;
+      scrollbar-width: none;
+      /* 40px refers to anchor-height */
+      scroll-padding: 2 * 40px;
+
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+
+    .toast {
+      top: 64px;
+      z-index: 1;
+    }
+
+    .hint {
+      z-index: 1;
+    }
+
+    .settings {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: 1;
+      display: grid;
+      gap: 16px;
+      align-content: start;
+      padding: 16px;
+      background-color: rgba(255, 255, 255, 0.95);
+
+      .option {
+        .label {
+          display: grid;
+          grid-auto-flow: column;
+          justify-content: space-between;
+          color: var(--color-text-normal);
+          font-size: 14px;
+        }
+
+        .description {
+          margin-top: 4px;
+          color: var(--color-text-light);
+          font-size: 12px;
+        }
+      }
+
+      hr {
+        margin: 0;
+        border: none;
+        border-top: 1px solid var(--color-bd);
+      }
+
+      .info {
+        display: grid;
+        gap: 8px;
+
+        .engine, .contact {
+          color: var(--color-text-normal);
+          font-size: 14px;
+
+          a {
+            color: var(--color-theme);
+          }
+        }
+      }
     }
   }
 
-  .loading {
-    display: grid;
-    grid-auto-flow: column;
-    gap: 4px;
-    align-items: center;
-    justify-content: center;
-    padding: 4px 0;
-    color: var(--color-text-light);
-    font-size: 14px;
-    cursor: pointer;
-
-    .icon {
-      font-size: 20px;
-    }
-  }
-
-  .scroll {
-    overflow-y: auto;
-    height: 540px;
-    scrollbar-width: none;
-    /* 40px refers to anchor-height */
-    scroll-padding: 2 * 40px;
-
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  }
-
-  .toast {
-    top: 64px;
-    z-index: 1;
-  }
-
-  .hint {
-    z-index: 1;
-  }
-
-  .settings {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1;
-    background-color: rgba(255, 255, 255, 0.95);
-    font-size: 16px;
-  }
 </style>

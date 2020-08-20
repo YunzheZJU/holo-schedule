b23158079f2f0e26b5d4d5faa8de6486551d3327
Yunzhe <yunzhe@zju.edu.cn>
2020-08-20 21:34:01 +0800

    #17 Fix wrong init order and ensure the store to init firstly.

--- src/background/store/index.js
+++ src/background/store/index.js
@@ -2,8 +2,4 @@ import createStore from './store'
 
 const store = createStore()
 
-store.init()
-  .catch(err => console.error(err))
-  .then(() => console.log('[store]Init OK.'))
-
 export default store

beefe96daf31e22961e8b96db81a70c1ad12639f
Yunzhe <yunzhe@zju.edu.cn>
2020-08-23 02:39:58 +0800

    Download settings on opening popup.

--- src/popup/index.js
+++ src/popup/index.js
@@ -6,6 +6,7 @@ import ToastPlugin from 'plugins/toast'
 import store from 'store'
 import Vue from 'vue'
 import Fragment from 'vue-fragment'
+import browser from 'webextension-polyfill'
 import App from './App.vue'
 import './global.less'
 
@@ -20,3 +21,6 @@ new Vue({
   store,
   render: h => h(App),
 })
+
+const { workflows: { downloadSettings } } = browser.extension.getBackgroundPage()
+downloadSettings()

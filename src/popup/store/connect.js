cc2b27dcda8a6432aac1b4f5f5d64e3cfb7b1fa2
Yunzhe <yunzhe@zju.edu.cn>
2020-09-01 12:03:46 +0800

    Ignore unwanted state changes when connecting to background store.

--- src/popup/store/connect.js
+++ src/popup/store/connect.js
@@ -5,6 +5,11 @@ let port
 const connect = store => {
   port = browser.runtime.connect({ name: 'store' })
   port.onMessage.addListener(({ key, value }) => {
+    // Ignore unwanted state changes
+    if (!Object.getOwnPropertyDescriptor(store.state, key)) {
+      return
+    }
+
     store.commit(key, value)
   })
 }

44c943ad2684fd68e6e69214dfe62306893f0b89
Sam <Bigmac@DESKTOP-O7HJNUL>
2021-04-13 23:35:21 +0200

    Added DE Language [v1.0] Added de json5 files edited index.js in option & popup [To make DE Language Usable]
    Debug Log:
     Translations : Works
     Functionality : Works
     Usability : Works
    
     Bugs:
    	- Issue Posted [https://github.com/YunzheZJU/holo-schedule/issues/64]
    
    Changes to be committed:
    	modified:   src/options/i18n/index.js
     	modified:   src/popup/i18n/index.js
    
    Untracked files:
    	src/_locales/de/
     	src/background/i18n/locales/de.json5
     	src/options/i18n/locales/de.json5
     	src/popup/i18n/locales/de.json5

--- src/options/i18n/index.js
+++ src/options/i18n/index.js
@@ -1,9 +1,10 @@
 import createI18n from 'shared/i18n'
 import store from 'store'
+import de from './locales/de.json5'
 import en from './locales/en.json5'
 import ja from './locales/ja.json5'
 import zhCN from './locales/zh-CN.json5'
 
-const i18n = createI18n(store, { en, ja, 'zh-CN': zhCN })
+const i18n = createI18n(store, { en, de, ja, 'zh-CN': zhCN })
 
 export default i18n

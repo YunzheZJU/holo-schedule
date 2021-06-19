6f115b5096be45707a745878af59792588fa4e64
Yunzhe <yunzhe@zju.edu.cn>
2021-06-19 17:41:01 +0800

    Add normalize to utils.

--- src/background/utils/index.js
+++ src/background/utils/index.js
@@ -1,5 +1,8 @@
 import dayjs from 'dayjs'
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import { clamp } from 'lodash'
+
+const { min, max } = Math
 
 dayjs.extend(isSameOrAfter)
 
@@ -13,4 +16,20 @@ const isGuerrillaLive = live => dayjs(live['created_at'])
   .add(10, 'minute')
   .isSameOrAfter(dayjs(live['start_at']))
 
-export { getUnixAfterDays, getUnixBeforeDays, getUnix, isGuerrillaLive }
+const normalize = (arrayOfObjects, ...keys) => keys.reduce((accu, key) => {
+  const filteredValues = accu.map(({ [key]: value }) => value).map(Number).filter(Boolean)
+  const minValue = min(...filteredValues)
+  const maxValue = max(...filteredValues)
+  return accu.map(object => ({
+    ...object,
+    [key]: (clamp(object[key], minValue, maxValue) - minValue) / (maxValue - minValue),
+  }))
+}, arrayOfObjects)
+
+export {
+  getUnixAfterDays,
+  getUnixBeforeDays,
+  getUnix,
+  isGuerrillaLive,
+  normalize,
+}

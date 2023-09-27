a0d245e8b6485f7d466eb20703c354e9c653b5fb
Yunzhe <yunzhe@zju.edu.cn>
2023-09-27 16:47:43 +0800

    Fix browser detection (#152)
    * Use IS_CHROME from env.
    
    * Use IS_CHROME from env.
    
    * Fix tests.

--- src/shared/browser/index.js
+++ src/shared/browser/index.js
@@ -2,6 +2,7 @@ import browser from 'webextension-polyfill'
 
 browser.action = browser.action ?? browser.browserAction
 browser.i18n.getUILanguage = browser.i18n.getUILanguage ?? (() => navigator?.language)
-browser.isChrome = typeof InstallTrigger === 'undefined'
+// eslint-disable-next-line no-undef
+browser.isChrome = IS_CHROME
 
 export default browser
--- webpack.config.js
+++ webpack.config.js
@@ -116,6 +116,7 @@ module.exports = (env, argv) => {
       }),
       new webpack.DefinePlugin({
         VERSION: JSON.stringify(PACKAGE.version),
+        IS_CHROME: JSON.stringify(isChrome),
         __VUE_OPTIONS_API__: true,
         __VUE_PROD_DEVTOOLS__: false,
         __VUE_I18N_FULL_INSTALL__: true,

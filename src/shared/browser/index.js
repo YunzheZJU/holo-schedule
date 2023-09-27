307bc0016cf89982e6f604e2c4de5690dffa8ac3
Yunzhe <yunzhe@zju.edu.cn>
2023-09-27 15:30:56 +0800

    Use IS_CHROME from env.

--- src/shared/browser/index.js
+++ src/shared/browser/index.js
@@ -3,6 +3,6 @@ import browser from 'webextension-polyfill'
 browser.action = browser.action ?? browser.browserAction
 browser.i18n.getUILanguage = browser.i18n.getUILanguage ?? (() => navigator?.language)
 // eslint-disable-next-line no-undef
-browser.isChrome = IS_CHROME === 'true'
+browser.isChrome = IS_CHROME
 
 export default browser
